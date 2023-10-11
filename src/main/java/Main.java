b990cc5b53d57c6ce83ae3de2d06458eea4d5e44
nayhub <antguajardo2021@udec.cl>
2023-10-11 14:51:02 -0300

    feat: creación clase Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Expendedor exp = new Expendedor(5,300);
+        Moneda m = null;
+        Comprador c=null;
+    }
 }
