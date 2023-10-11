5d11d44f7d23a2959593ca834e0be4d71621bed0
nayhub <antguajardo2021@udec.cl>
2023-10-11 14:47:44 -0300

    feat: creación clase Bebida

---
+++ src/main/java/Bebida.java
@@ -0,0 +1,6 @@
+public abstract class Bebida{
+    private int serie;
+    public Bebida(int serie){ this.serie = serie; }
+    public int getSerie() { return serie; }
+    public abstract String beber();
+}
