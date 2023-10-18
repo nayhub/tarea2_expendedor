5c1f9f1b9bb39b41f1d1c526192b900ef77199dd
Jhonweedmann <benjaminjr2004@gmail.com>
2023-10-18 10:48:36 -0300

    feat: Ahora moneda tiene la interfaz comparable para comparar monedas

--- src/main/java/Moneda.java
+++ src/main/java/Moneda.java
@@ -1,5 +1,11 @@
-public abstract class Moneda {
+public abstract class Moneda implements Comparable <Moneda> {
     public Moneda() { }
+
+    @Override
+    public int compareTo(Moneda o) {
+        return Integer.compare(getValor(),(o.getValor()));
+    }
+
     public Moneda getSerie() { return this; }
     public abstract int getValor();
 }
