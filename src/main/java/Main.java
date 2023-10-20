8b43060216be81a7e7e99d3bae16c79960fba680
nayhub <antguajardo2021@udec.cl>
2023-10-20 16:34:10 -0300

    style: eliminar algunos comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        /*
-
-        {
-            Expendedor exp = new Expendedor(5);
-            Moneda1000 m1000 = new Moneda1000();
-            Moneda100 m100 = new Moneda100();
-            Moneda500 m500 = new Moneda500();
-            Moneda1500 m1500 = new Moneda1500();
-
-            Comprador comprador = null;
-            try {
-                comprador = new Comprador(m1000, seleccionarProducto.super8.getOpcion(), exp);
-            } catch (PagoInsuficienteException e) {
-                throw new RuntimeException(e);
-            } catch (NoHayProductoException e) {
-                throw new RuntimeException(e);
-            }
-
-            /*
-            Comprador comprador = null;
-            try {
-                comprador = new Comprador(m1000, seleccionarProducto.coca.getOpcion(), exp);
-            } catch (PagoInsuficienteException e) {
-                throw new RuntimeException(e);
-            } catch (NoHayProductoException e) {
-                throw new RuntimeException(e);
-            }
-
-
-
-            System.out.println(comprador.queConsumiste() + "\n" + comprador.cuantoVuelto());
-
-            //Moneda m = null;
-            //Comprador c=null;
-            //exp.comprarProducto(m,3);
-        }
-
-         */
-
         Expendedor exp = new Expendedor(6);
         Moneda m = null;
         Comprador comprador = null;
