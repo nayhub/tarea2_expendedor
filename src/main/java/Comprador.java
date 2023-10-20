ebaf282fd556e3809f615129328670404869bb02
nayhub <antguajardo2021@udec.cl>
2023-10-20 16:34:31 -0300

    feat: Javadoc de Comprador

--- src/main/java/Comprador.java
+++ src/main/java/Comprador.java
@@ -1,7 +1,19 @@
+/**
+ * Clase que representa un comprador.
+ */
 public class Comprador {
     private String sonido;
     private Expendedor exp;
     private int vuelto;
+
+    /**
+     * Constructor de la clase Comprador.
+     * @param m La moneda utilizada para la compra.
+     * @param cualProducto El número del producto a comprar.
+     * @param exp El expendedor utilizado para la compra.
+     * @throws PagoInsuficienteException Si el pago es insuficiente para comprar el producto.
+     * @throws NoHayProductoException Si no hay suficientes unidades del producto para la venta.
+     */
     public Comprador(Moneda m, int cualProducto, Expendedor exp) throws PagoInsuficienteException, NoHayProductoException{
         this.exp = exp;
         Producto a = exp.comprarProducto(m, cualProducto);
@@ -17,6 +29,14 @@ public class Comprador {
             else b = false;
         }
     }
+    /**
+     * Método que devuelve el vuelto del comprador.
+     * @return El vuelto del comprador.
+     */
     public int cuantoVuelto(){ return vuelto; }
+    /**
+     * Método que devuelve el nombre del producto consumido por el comprador.
+     * @return El nombre del producto consumido por el comprador.
+     */
     public String queConsumiste(){ return sonido; }
 }
