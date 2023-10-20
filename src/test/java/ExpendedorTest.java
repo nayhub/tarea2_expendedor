31fa07b6d2128e6a610726c278ac2f53a631c0fd
nayhub <antguajardo2021@udec.cl>
2023-10-19 22:43:16 -0300

    feat: tests para clare Expendedor

--- src/test/java/ExpendedorTest.java
+++ src/test/java/ExpendedorTest.java
@@ -1,6 +1,34 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ExpendedorTest {
+public class ExpendedorTest {
+    @Test
+    public void testComprarProducto() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
+        Expendedor exp = new Expendedor(10);
+        Moneda m = new Moneda1000();
+        Producto p = exp.comprarProducto(m, 1);
+        assertNotNull(p);
+        assertEquals(CocaCola.class, p.getClass());
+    }
+
+    @Test
+    public void testComprarProductoNoHayProducto() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
+        Expendedor exp = new Expendedor(0);
+        Moneda m = new Moneda100();
+        assertThrows(PagoInsuficienteException.class, () -> exp.comprarProducto(m, 1));
+    }
+
+    @Test
+    public void testComprarProductoPagoIncorrecto() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
+        Expendedor exp = new Expendedor(10);
+        Moneda m = null;
+        assertThrows(PagoIncorrectoException.class, () -> exp.comprarProducto(m, 1));
+    }
+
+    @Test
+    public void testComprarProductoPagoInsuficiente() throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
+        Expendedor exp = new Expendedor(3);
+        Moneda m = new Moneda100();
+        assertThrows(PagoInsuficienteException.class, () -> exp.comprarProducto(m, 5));
+    }
 }
